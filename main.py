872498610891a8436f290cd02e85638036e03e6d
ProCryptor <96250392+ProCryptor@users.noreply.github.com>
2025-12-24 17:59:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -117,7 +117,7 @@ async def process_route(route: Route) -> None:
 
     private_key = route.wallet.private_key
 
-        is_bridge_day = planner.is_bridge_day(day_type)
+    is_bridge_day = planner.is_bridge_day(day_type)
 
     if not memory.can_bridge_today(wallet_id):
         logger.info('Memory: bridge cooldown active')
