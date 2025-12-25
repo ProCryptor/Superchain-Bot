d0178f5c8827f522a62497e4ed3caeb3f146cf04
ProCryptor <96250392+ProCryptor@users.noreply.github.com>
2025-12-25 14:11:51 +0100

    Update retrieve_route.py

--- src/utils/retrieve_route.py
+++ src/utils/retrieve_route.py
@@ -1,5 +1,4 @@
 from typing import List, Optional
-
 from loguru import logger
 
 from src.database.base_models.pydantic_manager import DataBaseManagerConfig
@@ -7,32 +6,27 @@ from src.database.utils.db_manager import DataBaseUtils
 from src.models.route import Route, Wallet
 
 
-async def get_routes(private_keys: str) -> Optional[List[Route]]:
+async def get_routes(private_keys) -> Optional[List[Route]]:
     db_utils = DataBaseUtils(
-        manager_config=DataBaseManagerConfig(
-            action='working_wallets'
-        )
+        manager_config=DataBaseManagerConfig(action='working_wallets')
     )
-    result = await db_utils.get_uncompleted_wallets()
-    if not result:
-        logger.success(f'Все кошельки с данной базы данных уже отработали')
+
+    wallets = await db_utils.get_uncompleted_wallets()
+    if not wallets:
+        logger.success('Все кошельки уже отработали')
         return None
 
     routes = []
-    for wallet in result:
-        for private_key in private_keys:
-            if wallet.private_key.lower() == private_key.lower():
-                private_key_tasks = await db_utils.get_wallet_pending_tasks(private_key)
-                tasks = []
-                for task in private_key_tasks:
-                    tasks.append(task.task_name)
-                routes.append(
-                    Route(
-                        tasks=tasks,
-                        wallet=Wallet(
-                            private_key=private_key,
-                            proxy=wallet.proxy,
-                        )
-                    )
-                )
+    for wallet in wallets:
+        routes.append(
+            Route(
+                wallet=Wallet(
+                    private_key=wallet.private_key,
+                    proxy=wallet.proxy,
+                ),
+                tasks=[]  # больше не используется
+            )
+        )
+
     return routes
+
