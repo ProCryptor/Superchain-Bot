367fd3fc065299f455b2f2796469d49db2071599
ProCryptor <96250392+ProCryptor@users.noreply.github.com>
2025-12-25 22:42:46 +0100

    Update chain.py

--- src/models/chain.py
+++ src/models/chain.py
@@ -1,10 +1,14 @@
+# src/models/chain.py
 from __future__ import annotations
-
 from pydantic import BaseModel
 
 
 class Chain(BaseModel):
     chain_name: str
-    native_token: str
-    rpc: str
     chain_id: int
+    rpc: str
+    scan: str
+    native_token: str
+
+    class Config:
+        arbitrary_types_allowed = True
